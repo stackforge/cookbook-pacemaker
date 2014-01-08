a19cca3b683ce87ffbbe563c94ca93adcf804f4f
Adam Spiers <aspiers@suse.com>
2014-01-08 20:53:36 +0000

    active nodes are managed by pacemaker, not chef

--- attributes/default.rb
+++ attributes/default.rb
@@ -28,13 +28,11 @@ default['pacemaker']['primitive']['cinder-volume']['agent'] = "ocf:openstack:cin
 default['pacemaker']['primitive']['cinder-volume']['meta']['is-managed'] = "true"
 default['pacemaker']['primitive']['cinder-volume']['meta']['target-role'] = "Started"
 default['pacemaker']['primitive']['cinder-volume']['op']['monitor']['interval'] = "10s"
-default['pacemaker']['primitive']['cinder-volume']['active'] = "#{pacemaker['nodes']}"
 
 default['pacemaker']['primitive']['clvm']['agent'] = "ocf:lvm2:clvmd"
 default['pacemaker']['primitive']['clvm']['params']['daemon_timeout'] = "30"
 default['pacemaker']['primitive']['clvm']['op']['monitor']['interval'] = "5s"
 default['pacemaker']['primitive']['clvm']['op']['monitor']['on-fail'] = "restart"
-default['pacemaker']['primitive']['clvm']['active'] = "#{pacemaker['nodes']}"
 
 # Vip address might be replaced by cinder-volume's myip specified in the environment.
 default['pacemaker']['primitive']['vip']['agent'] = "ocf:heartbeat:IPaddr2"
@@ -43,23 +41,17 @@ default['pacemaker']['primitive']['vip']['params']['cidr_netmask'] = "24"
 default['pacemaker']['primitive']['vip']['op']['monitor']['interval'] = "3s"
 default['pacemaker']['primitive']['vip']['op']['monitor']['nic'] = "eth0"
 default['pacemaker']['primitive']['vip']['meta']['target-role'] = "Started"
-default['pacemaker']['primitive']['vip']['active'] = "#{pacemaker['nodes']}"
 
 default['pacemaker']['location']['l-st-node1']['rsc_name'] = "st-node1"
 default['pacemaker']['location']['l-st-node1']['priority'] = "-inf"
-default['pacemaker']['location']['l-st-node1']['loc'] = "#{pacemaker['nodes'][0]}"
-default['pacemaker']['location']['l-st-node1']['active'] = ["#{pacemaker['nodes'][1]}"]
 
 default['pacemaker']['location']['l-st-node2']['rsc_name'] = "st-node2"
 default['pacemaker']['location']['l-st-node2']['priority'] = "-inf"
-default['pacemaker']['location']['l-st-node2']['loc'] = "#{pacemaker['nodes'][1]}"
-default['pacemaker']['location']['l-st-node2']['active'] = ["#{pacemaker['nodes'][0]}"]
 
 default['pacemaker']['clone']['clvm-clone']['rsc_name'] = "clvm"
 default['pacemaker']['clone']['clvm-clone']['meta']['globally-unique'] = "false"
 default['pacemaker']['clone']['clvm-clone']['meta']['interleave'] = "true"
 default['pacemaker']['clone']['clvm-clone']['meta']['ordered'] = "true"
-default['pacemaker']['clone']['clvm-clone']['active'] = "#{pacemaker['nodes']}"
 
 default['pacemaker']['colocation']['c-cinder-volume']['priority'] = "inf"
 default['pacemaker']['colocation']['c-cinder-volume']['is_multiple'] = "yes"
@@ -70,10 +62,8 @@ default['pacemaker']['colocation']['c-cinder-volume']['with_rsc'] = nil
 
 # Multiple colocation (if multiple is 'yes')
 default['pacemaker']['colocation']['c-cinder-volume']['multiple_rscs'] = ['drbd-cluster', 'vip', 'cinder-volume']
-default['pacemaker']['colocation']['c-cinder-volume']['active'] = "#{pacemaker['nodes']}"
 
 default['pacemaker']['order']['o-lvm']['priority'] = "inf"
 default['pacemaker']['order']['o-lvm']['resources'] = ['drbd-cluster', 'clvm-clone', 'vip', 'cinder-volume']
-default['pacemaker']['order']['o-lvm']['active'] = "#{pacemaker['nodes']}"
 
 default['pacemaker']['property']['no-quorum-policy'] = "ignore"
