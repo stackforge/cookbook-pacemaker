54c66607a2184261a2b07a477d640a832b7e9303
Adam Spiers <aspiers@suse.com>
2014-01-23 12:26:42 +0000

    allow spec filtering

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,4 +33,7 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus => true
 end
