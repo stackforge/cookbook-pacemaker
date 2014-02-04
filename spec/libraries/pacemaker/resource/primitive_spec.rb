c21a592ab18f461d47b1c8f8bba2c30f73f72992
Adam Spiers <aspiers@suse.com>
2014-02-04 17:38:57 +0000

    extract fixture_definition variable

--- spec/libraries/pacemaker/resource/primitive_spec.rb
+++ spec/libraries/pacemaker/resource/primitive_spec.rb
@@ -5,6 +5,9 @@ require File.join(File.dirname(__FILE__), %w(.. .. .. helpers common_object_exam
 
 describe Pacemaker::Resource::Primitive do
   let(:fixture) { Chef::RSpec::Pacemaker::Config::KEYSTONE_PRIMITIVE.dup }
+  let(:fixture_definition) {
+    Chef::RSpec::Pacemaker::Config::KEYSTONE_PRIMITIVE_DEFINITION
+  }
 
   before(:each) do
     Mixlib::ShellOut.any_instance.stub(:run_command)
@@ -105,15 +108,14 @@ describe Pacemaker::Resource::Primitive do
 
   describe "#definition_string" do
     it "should return the definition string" do
-      expect(fixture.definition_string).to \
-        eq(Chef::RSpec::Pacemaker::Config::KEYSTONE_PRIMITIVE_DEFINITION)
+      expect(fixture.definition_string).to eq(fixture_definition)
     end
   end
 
   describe "#parse_definition" do
     before(:each) do
       @parsed = Pacemaker::Resource::Primitive.new(fixture.name)
-      @parsed.definition = Chef::RSpec::Pacemaker::Config::KEYSTONE_PRIMITIVE_DEFINITION
+      @parsed.definition = fixture_definition
       @parsed.parse_definition
     end
 
