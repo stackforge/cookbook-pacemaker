cb0d3adfb05384cb6f17a8b09beb71bd213e14d1
Adam Spiers <aspiers@suse.com>
2014-02-06 18:47:57 +0000

    define TYPE for clone

--- libraries/pacemaker/resource/clone.rb
+++ libraries/pacemaker/resource/clone.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../resource', File.dirname(__FILE__))
 
 class Pacemaker::Resource::Clone < Pacemaker::Resource
-  register_type 'clone'
+  TYPE = 'clone'
+  register_type TYPE
 
   attr_accessor :primitive
 
