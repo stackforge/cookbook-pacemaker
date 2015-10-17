5f8b586576f8a2d4c61beb437eed4a1ef99009bd
Monty Taylor <mordred@inaugust.com>
2015-10-17 16:02:55 -0400

    Retire stackforge/cookbook-pacemaker

--- libraries/chef/mixin/pacemaker.rb
+++
@@ -1,3 +0,0 @@
-this_dir = File.dirname(__FILE__)
-require File.expand_path('pacemaker/standard_cib_object.rb', this_dir)
-require File.expand_path('pacemaker/runnable_resource.rb',   this_dir)
--- metadata.rb
+++
@@ -1,9 +0,0 @@
-name            "pacemaker"
-maintainer       "Crowbar Project"
-maintainer_email "crowbar@dell.com"
-license          "Apache 2.0"
-description      "Installs/configures Pacemaker"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1"
-
-depends "corosync"
